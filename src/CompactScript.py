84c53f0d7d42af1959161938608ff78e8956d910
luochengleo <luochengleo@gmail.com>
2014-07-13 14:12:57 +0800

    fafa

---
+++ src/CompactScript.py
@@ -0,0 +1,6 @@
+import os
+
+fout = open('../data/compact.sh','w')
+for f in os.listdir('../data/querylog'):
+    fout.write('nohup 7z a ./querylog_7z/'+f+'.7z ./querylog/'+f+' > ./log/'+f+'.log &')
+fout.close()
