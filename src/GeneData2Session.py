8f8d22eae78bc083ab764aff292108e881b4d91f
luochengleo <luochengleo@gmail.com>
2014-07-14 16:50:36 +0800

    aff

--- src/GeneData2Session.py
+++ src/GeneData2Session.py
@@ -1,4 +1,4 @@
 fout = open('runData2Session.sh','w')
 for i in range(0,24,1):
-    fout.write('nohup python Data2Session '+str(i)+' > ../data/log/'+str(i)+'.log &\n')
+    fout.write('nohup python Data2Session.py '+str(i)+' > ../data/log/'+str(i)+'.log &\n')
 fout.close()
