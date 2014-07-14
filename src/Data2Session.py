32608ff7f3fbf0241aacd122fafd8ef0036db10f
luochengleo <luochengleo@gmail.com>
2014-07-14 15:41:15 +0800

    fafa

--- src/Data2Session.py
+++ src/Data2Session.py
@@ -1,7 +1,7 @@
 import sys
 
 
-fin = open('../data/querylog/'+sys.argv[1]+'.dat')
+fin = open('../data/querylogbyid/'+sys.argv[1]+'.dat')
 line = fin.readline()
 count = 0
 currtime = 0
@@ -12,16 +12,16 @@ while line != '':
         print count
     try:
         segs = line.strip().split('\t')
-        if len(segs) ==3:
-            query = segs[0]
-            sessionid= segs[1]
-            dataOfSession[sessionid] = list()
+        
+        sessionid= segs[1]
+        
+        dataOfSession[sessionid] = list()
+        
     except:
         print line
     
     line = fin.readline()
 fin.close()
-
 
 def session2text(lt):
     text = ''
@@ -29,41 +29,70 @@ def session2text(lt):
         text = text+' '+q
     return text
 
-
-fin = open('../data/querylog/'+sys.argv[1]+'.dat')
+fin = open('../data/querylogbyid/'+sys.argv[1]+'.dat')
 fout = open('../data/sessiontext/'+sys.argv[1]+'.txt','w')
 line = fin.readline()
 count = 0
 currtime = 0
 while line != '':
     count +=1
-    if count %1000000==0:
-        print float(count)/160000000.0,' clear'
-        for k in dataOfSession.keys():
-            existData = dataOfSession[k]
+#     if count %1000000==0:
+#         print float(count)/160000000.0,' clear'
+#         for k in dataOfSession.keys():
+#             existData = dataOfSession[k]
+#             if len(existData)>0:
+#                 existTime = existData[-1][2]
+#                 if currtime - existTime > 1800.0:
+#                     fout.write(session2text(existData)+'\n')
+#                     dataOfSession[k] = list()
+    segs = line.strip().split('\t')
+
+    if len(segs) ==4 :
+        if segs[0]=='P':
+            
+            type = segs[0]
+            sessionid= segs[1]
+            time = float(segs[2])
+            query = segs[3]
+
+            currtime = time
+            existData = dataOfSession[sessionid]
             if len(existData)>0:
                 existTime = existData[-1][2]
                 if currtime - existTime > 1800.0:
                     fout.write(session2text(existData)+'\n')
-                    dataOfSession[k] = list()
-    segs = line.strip().split('\t')
-    if len(segs) ==3:
-        query = segs[0]
-        sessionid= segs[1]
-        time = float(segs[2])
-        currtime = time
-        existData = dataOfSession[sessionid]
-        if len(existData)>0:
-            existTime = existData[-1][2]
-            if currtime - existTime > 1800.0:
-                fout.write(session2text(existData)+'\n')
-                dataOfSession[sessionid] = list()
-                dataOfSession[sessionid].append((query,sessionid,time))
+                    dataOfSession[sessionid] = list()
+                    dataOfSession[sessionid].append(('P@'+query,sessionid,time))
+                else:
+                    dataOfSession[sessionid].append(('P@'+query,sessionid,time))
+            if len(existData) == 0:
+                dataOfSession[sessionid].append(('P@'+query,sessionid,time))
+    if len(segs) == 5:
+        if segs[0] == 'C':
+            type = segs[0]
+            sessionid= segs[1]
+            time = float(segs[2])
+            query = segs[3]
+            url=segs[4]
+
+            currtime = time
+            existData = dataOfSession[sessionid]
+            
+            if 'www.sogou.com' in url:
+                bullet = ('P@'+query,sessionid,time)
             else:
-                dataOfSession[sessionid].append((query,sessionid,time))
-        if len(existData) == 0:
-            dataOfSession[sessionid].append((query,sessionid,time))
-    
+                bullet = ('C@'+url,sessionid,time)
+                
+            if len(existData)>0:
+                existTime = existData[-1][2]
+                if currtime - existTime > 1800.0:
+                    fout.write(session2text(existData)+'\n')
+                    dataOfSession[sessionid] = list()
+                    dataOfSession[sessionid].append(bullet)
+                else:
+                    dataOfSession[sessionid].append(bullet)
+            if len(existData) == 0:
+                dataOfSession[sessionid].append(bullet)
     line = fin.readline()
 fin.close()
 
