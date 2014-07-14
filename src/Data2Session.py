e52ff9dee625a0e6d374c4902d50362af7e1c0cc
luochengleo <luochengleo@gmail.com>
2014-07-14 16:48:48 +0800

    fa

--- src/Data2Session.py
+++ src/Data2Session.py
@@ -1,102 +1,106 @@
 import sys
 
+
+mod = int(sys.argv[1])
+
 for idx in range(0,1024,1):
-    print idx
-    fin = open('../data/querylogbyid/'+str(idx)+'.dat')
-    line = fin.readline()
-    count = 0
-    currtime = 0
-    dataOfSession = dict()
-    while line != '':
-        count +=1
-        if count %10000000==0:
-            print count
-        try:
-            segs = line.strip().split('\t')
-            sessionid= segs[1]
-            dataOfSession[sessionid] = list()
-        except:
-            print line
+    if idx%1024==mod:
+        print idx
+        fin = open('../data/querylogbyid/'+str(idx)+'.dat')
+        line = fin.readline()
+        count = 0
+        currtime = 0
+        dataOfSession = dict()
+        while line != '':
+            count +=1
+            if count %10000000==0:
+                print count
+            try:
+                segs = line.strip().split('\t')
+                sessionid= segs[1]
+                dataOfSession[sessionid] = list()
+            except:
+                print line
+            
+            line = fin.readline()
+        fin.close()
         
+        def session2text(lt):
+            text = ''
+            for (q,s,t) in lt:
+                text = text+' '+q
+            return text
+        
+        fin = open('../data/querylogbyid/'+str(idx)+'.dat')
+        fout = open('../data/sessiontext/'+str(idx)+'.txt','w')
         line = fin.readline()
-    fin.close()
-    
-    def session2text(lt):
-        text = ''
-        for (q,s,t) in lt:
-            text = text+' '+q
-        return text
-    
-    fin = open('../data/querylogbyid/'+str(idx)+'.dat')
-    fout = open('../data/sessiontext/'+str(idx)+'.txt','w')
-    line = fin.readline()
-    count = 0
-    currtime = 0
-    while line != '':
-        count +=1
-    #     if count %1000000==0:
-    #         print float(count)/160000000.0,' clear'
-    #         for k in dataOfSession.keys():
-    #             existData = dataOfSession[k]
-    #             if len(existData)>0:
-    #                 existTime = existData[-1][2]
-    #                 if currtime - existTime > 1800.0:
-    #                     fout.write(session2text(existData)+'\n')
-    #                     dataOfSession[k] = list()
-        segs = line.strip().split('\t')
-    
-        if len(segs) ==4 :
-            if segs[0]=='P':
-                
-                type = segs[0].replace(' ','')
-                sessionid= segs[1].replace(' ','')
-                time = float(segs[2])
-                query = segs[3].replace(' ','')
-    
-                currtime = time
-                existData = dataOfSession[sessionid]
-                if len(existData)>0:
-                    existTime = existData[-1][2]
-                    if currtime - existTime > 1800.0:
-                        fout.write(session2text(existData)+'\n')
-                        dataOfSession[sessionid] = list()
-                        dataOfSession[sessionid].append(('P@'+query,sessionid,time))
-                    else:
-                        dataOfSession[sessionid].append(('P@'+query,sessionid,time))
-                if len(existData) == 0:
-                    dataOfSession[sessionid].append(('P@'+query,sessionid,time))
-        if len(segs) == 5:
-            if segs[0] == 'C':
-                type = segs[0].replace(' ','')
-                sessionid= segs[1].replace(' ','')
-                time = float(segs[2])
-                query = segs[3].replace(' ','')
-                url=segs[4].replace(' ','')
-    
-                currtime = time
-                existData = dataOfSession[sessionid]
-                
-                if 'www.sogou.com' in url:
-                    bullet = ('P@'+query,sessionid,time)
-                else:
-                    bullet = ('C@'+url,sessionid,time)
+        count = 0
+        currtime = 0
+        while line != '':
+            count +=1
+        #     if count %1000000==0:
+        #         print float(count)/160000000.0,' clear'
+        #         for k in dataOfSession.keys():
+        #             existData = dataOfSession[k]
+        #             if len(existData)>0:
+        #                 existTime = existData[-1][2]
+        #                 if currtime - existTime > 1800.0:
+        #                     fout.write(session2text(existData)+'\n')
+        #                     dataOfSession[k] = list()
+            segs = line.strip().split('\t')
+        
+            if len(segs) ==4 :
+                if segs[0]=='P':
                     
-                if len(existData)>0:
-                    existTime = existData[-1][2]
-                    if currtime - existTime > 1800.0:
-                        fout.write(session2text(existData)+'\n')
-                        dataOfSession[sessionid] = list()
-                        dataOfSession[sessionid].append(bullet)
+                    type = segs[0].replace(' ','')
+                    sessionid= segs[1].replace(' ','')
+                    time = float(segs[2])
+                    query = segs[3].replace(' ','')
+        
+                    currtime = time
+                    existData = dataOfSession[sessionid]
+                    if len(existData)>0:
+                        existTime = existData[-1][2]
+                        if currtime - existTime > 1800.0:
+                            fout.write(session2text(existData)+'\n')
+                            dataOfSession[sessionid] = list()
+                            dataOfSession[sessionid].append(('P@'+query,sessionid,time))
+                        else:
+                            dataOfSession[sessionid].append(('P@'+query,sessionid,time))
+                    if len(existData) == 0:
+                        dataOfSession[sessionid].append(('P@'+query,sessionid,time))
+            if len(segs) == 5:
+                if segs[0] == 'C':
+                    type = segs[0].replace(' ','')
+                    sessionid= segs[1].replace(' ','')
+                    time = float(segs[2])
+                    query = segs[3].replace(' ','')
+                    url=segs[4].replace(' ','')
+        
+                    currtime = time
+                    existData = dataOfSession[sessionid]
+                    
+                    if 'www.sogou.com' in url:
+                        bullet = ('P@'+query,sessionid,time)
                     else:
+                        bullet = ('C@'+url,sessionid,time)
+                        
+                    if len(existData)>0:
+                        existTime = existData[-1][2]
+                        if currtime - existTime > 1800.0:
+                            fout.write(session2text(existData)+'\n')
+                            dataOfSession[sessionid] = list()
+                            dataOfSession[sessionid].append(bullet)
+                        else:
+                            dataOfSession[sessionid].append(bullet)
+                    if len(existData) == 0:
                         dataOfSession[sessionid].append(bullet)
-                if len(existData) == 0:
-                    dataOfSession[sessionid].append(bullet)
-        line = fin.readline()
-    fin.close()
-    
-    for k in dataOfSession.keys():
-        existData = dataOfSession[k]
-        if len(existData)>0:
-            fout.write(session2text(existData)+'\n')
-            dataOfSession[k] = list()
-    fout.close()
+            line = fin.readline()
+        fin.close()
+        
+        for k in dataOfSession.keys():
+            existData = dataOfSession[k]
+            if len(existData)>0:
+                fout.write(session2text(existData)+'\n')
+                dataOfSession[k] = list()
+        fout.close()
---
+++ src/GeneData2Session.py
@@ -0,0 +1,4 @@
+fout = open('runData2Session.sh','w')
+for i in range(0,24,1):
+    fout.write('nohup python Data2Session '+str(i)+' > ../data/log/'+str(i)+'.log &\n')
+fout.close()
