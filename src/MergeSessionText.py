72fd9ede3b04fbef8ccc058da6a2bd9c10904d90
luochengleo <luochengleo@gmail.com>
2014-07-15 21:41:51 +0800

    fafaaf

--- src/MergeSessionText.py
+++ src/MergeSessionText.py
@@ -1,5 +1,8 @@
 for i in range(0,1024,1):
-    fout = open('../data/sessionmerge.txt','a')
-    print i
-    fout.write(open('../data/sessiontext/'+str(i)+'.txt').read())
+    try:
+        fout = open('../data/sessionmerge.txt','a')
+        print i
+        fout.write(open('../data/sessiontext/'+str(i)+'.txt').read())
     
+    except:
+        print i
